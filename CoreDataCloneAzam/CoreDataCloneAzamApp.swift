32cdab3c3d115be65434cf1ee8037711f06b3802
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-18 15:48:27 +0000

    Initial Commit

---
+++ CoreDataCloneAzam/CoreDataCloneAzamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataCloneAzamApp.swift
+//  CoreDataCloneAzam
+//
+//  Created by Kyungyun Lee on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataCloneAzamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
